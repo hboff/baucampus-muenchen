b0c8ab3c96295c9b83f3055514e6f6932ba2a9b5
Oskar Lohse <oskar@baucampus.de>
2023-07-03 10:41:41 +0200

    test 12

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -890,8 +890,16 @@
       </div>
     </div>
   </section>
-
-@include('partials.footer')
+  @php
+                $i=0;
+                @endphp        
+   
+    @foreach($ortat as $orte)
+    @if ($i++ < 16)
+  <a href="/baugutachter" class="list-item-footer w-inline-block">Baugutachter {{$orte->ort}}</a>
+    @else
+    @endif
+    @endforeach
 
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=648d0891096d52bfd9f271dc" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script src="js/webflow.js" type="text/javascript"></script>
